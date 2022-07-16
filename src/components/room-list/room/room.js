db08b828c0380a07d7e6a94f6e62e31a4190ea37
sophiiasun <53122458+sophiiasun@users.noreply.github.com>
2022-07-16 13:17:58 -0400

    changes before reinstall

--- src/components/room-list/room/room.js
+++ src/components/room-list/room/room.js
@@ -9,7 +9,7 @@ function Room({Key, RouteChange}) {
 	return (
 		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5' onClick = {changePage}>
 		<img alt='chat-room' src={'https://i.pinimg.com/originals/14/db/65/14db65db9599cd2d5d7aa967e73031a3.jpg'}/>
-		<div>
+		<div >
 			<h2>Topic</h2>
 			<p>Topic details</p>
 		</div>
